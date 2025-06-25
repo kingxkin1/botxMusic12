3ad74c21cd6fab7a789d4676801a89ec351b441f
kingxkin1 <bairagitg@gmail.com>
2025-06-25 22:45:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/shizukamusicbot1/ShizukaMusicBot",
+    "https://github.com/kingxkin1/botxMusic122",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
